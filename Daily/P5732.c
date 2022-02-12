ac0ef61c95da8e62455c85b8babc1e0f89932e3c
江渚 <93872575+JiangZhu-N@users.noreply.github.com>
2022-02-12 13:00:52 +0000

    Create P5732.c

---
+++ Daily/P5732.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[200][200];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0)
+            {
+                a[i][j] = 1;
+            }
+            else if (j == i)
+            {
+                a[i][j] = 1;
+            }
+            else
+            {
+                a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
